4ad54729fc6abf177a7b59acb492c1e0243b10c0
Wh1t3Fox <dev@exploit.design>
2020-12-18 21:41:54 -0500

    bump dep versions

--- rete/__init__.py
+++ rete/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
 
-from pathlib import Path
-import coloredlogs
 import logging
 import logging.config
+from rich.logging import RichHandler
+from pathlib import Path
 import subprocess
 import os
 
@@ -56,41 +56,9 @@ if not os.path.exists(pulse_socket):
 
 BROWSERS = ["brave", "chromium", "firefox", "opera", "tbb"]
 
-coloredlogs.install()
-logging.config.dictConfig(
-    {
-        "version": 1,
-        "disable_existing_loggers": True,
-        "loggers": {"": {"level": "DEBUG", "handlers": ["console", "file"]}},
-        "formatters": {
-            "colored_console": {
-                "()": "coloredlogs.ColoredFormatter",
-                "format": "%(levelname)s %(message)s",
-                "datefmt": "%H:%M:%S",
-            },
-            "file_format": {
-                "format": "%(asctime)s :: %(funcName)s in %(filename)s (l:%(lineno)d) :: %(message)s",
-                "datefmt": "%Y-%m-%d %H:%M:%S",
-            },
-        },
-        "handlers": {
-            "console": {
-                "level": "INFO",
-                "class": "logging.StreamHandler",
-                "formatter": "colored_console",
-                "stream": "ext://sys.stdout",
-            },
-            "file": {
-                "level": "DEBUG",
-                "formatter": "file_format",
-                "class": "logging.FileHandler",
-                "filename": f"{USER_DATA_PATH}/rete.log",
-            },
-        },
-    }
+logging.basicConfig(
+    level='INFO',
+    format='%(message)s',
+    datefmt='[%X]',
+    handlers=[RichHandler(rich_tracebacks=True)]
 )
-
-logging.addLevelName(logging.DEBUG, "[+]")
-logging.addLevelName(logging.INFO, "[i]")
-logging.addLevelName(logging.WARNING, "[!]")
-logging.addLevelName(logging.ERROR, "[-]")
